a90e8e6add70a9c65e052bad959e309b3dd52055
TrixCal <traytonlarson01@gmail.com>
2022-02-21 12:53:12 -0600

    Added ToString method to movie.cs

--- Movie.cs
+++ Movie.cs
@@ -1,16 +1,26 @@
 using System;
-
+using System.Collections.Generic;
 namespace MovieLibrary
 {
     class Movie
     {
-        int movieId;
-        string title;
-        List<string> genres;
+        public int movieId {get; set;}
+        public string title {get; set;}
+        public List<string> genres {get; set;}
+        public Movie(){
+            genres = new List<string>();
+        }
         public Movie(int MovieId, string Title, List<string> Genres){
             movieId = MovieId;
             title = Title;
             genres = Genres;
+        }
+        public override string ToString(){
+            string genre = "";
+            foreach(string s in genres){
+                genre = genre + " | "  + s;
+            }
+            return $"{title}, {genre}";
         }
     }
 }
